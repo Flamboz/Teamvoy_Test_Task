5c7309b53a31b8fdaeb4b30f1d23b7ca30db94ef
nadiia ostapyshyn <nadia0ostapyshyn@gmail.com>
2022-08-30 22:20:40 +0300

    feat: fetch data and add CardsGallery

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CardsGallery from '../CardsGallery/CardsGallery';
 import styles from './Pokedex.module.css';
 
-const Pokedex = () => (
-  <section>
-    <h1 className={styles.title}>Pokedex</h1>
-  </section>
-);
+const Pokedex = () => {
+  const [pokemonsData, setPokemonsData] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15');
+      const data = await res.json();
+      setPokemonsData(data.results);
+    }
+
+    loadData();
+  }, []);
+
+  return (
+    <section>
+      <h1 className={styles.title}>Pokedex</h1>
+      <CardsGallery pokemonsData={pokemonsData} />
+    </section>
+  );
+};
 
 export default Pokedex;
